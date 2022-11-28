4f315f339da639827185b20c853426fe0a546228
cunning <76720185+cunning-sys@users.noreply.github.com>
2022-11-28 09:04:17 -0800

    Update buyaura.lua

--- buyaura.lua
+++ buyaura.lua
@@ -1,9 +1,14 @@
+local distanceCheck = true
+local dist = 10
+local check = true
+
+
 function getClosestBuyPart()
   local character = game.Players.LocalPlayer.Character
   local hrp = character:FindFirstChild("HumanoidRootPart") or character:WaitForChild("HumanoidRootPart")
   if not (character or hrp) then return end
   
-  local distance = math.huge
+  local distance = if distanceCheck then return dist else return math.huge end
   local part
   
   for i,v in pairs(game.Workspace.Ignored.Shop:GetChildren()) do
@@ -19,5 +24,7 @@ function getClosestBuyPart()
 end
 
 RS.Heartbeat:Connect(function()
-    
+    local target = getClosestBuyPart()
+    if check and game.Players.LocalPlayer.Character:FindFirstChild("") or game.Players.LocalPlayer.Backpack:FindFirstChild("") then
+      
 end)
