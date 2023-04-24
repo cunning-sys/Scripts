e854a271cab4053b736e2f6b9f127d0a620282c9
cunning <76720185+cunning-sys@users.noreply.github.com>
2023-04-24 11:24:54 -0700

    lol

--- closest_player_anti_lock.lua
+++ closest_player_anti_lock.lua
@@ -1,1 +1,27 @@
+local Players = game:GetService('Players')
+local LocalPlayer = Players.LocalPlayer
 
+local get_closest_player = function()
+    local dist = math.huge
+    local cplayer = nil
+
+    for i, player in ipairs(Players:GetPlayers()) do
+        if player.Character and player ~= LocalPlayer then
+            local pos = player.Character:GetBoundingBox().Position
+            if pos then
+                local mag = (pos - LocalPlayer.Character:GetBoundingBox().Position).Magnitude
+                if mag < dist then
+                    cplayer = player
+                    dist = mag
+                end
+            end
+        end
+    end
+    return cplayer
+end
+
+game:GetService('RunService').Heartbeat:Connect(function()
+    local closest_player = get_closest_player()
+
+    LocalPlayer.Character.HumanoidRootPart.Velocity = closest_player.Character.HumanoidRootPart.Position + closest_player.Character.HumanoidRootPart.Velocity - LocalPlayer.Character.HumanoidRootPart.Position
+end)
